e5529a87e70600b82ba32e6997a867bb6d9bd8df
Viacheslav Iakovlev <jakovlev.vya4eslaw@yandex.ru>
2026-03-05 19:59:17 +0500

    refactor: remove unnecessary import and add @Slf4j to ErrorResponse DTO

--- src/main/java/com/viacheslav/taskmanager/dto/ErrorResponse.java
+++ src/main/java/com/viacheslav/taskmanager/dto/ErrorResponse.java
@@ -4,11 +4,11 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Builder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 
+@Slf4j
 @Builder
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record ErrorResponse(
@@ -18,8 +18,6 @@ public record ErrorResponse(
         List<FieldErrorDetail> errors,
         String path
 ) {
-
-    private static final Logger log = LoggerFactory.getLogger(ErrorResponse.class);
 
     public record FieldErrorDetail(
             String field,
